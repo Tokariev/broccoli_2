7307903cded1dea94460b66845578e4c1e1dd6dc
antontyutyunnik <sumytoxa@ukr.net>
2018-05-03 18:01:08 +0300

    сделал авторизацию как обсуждали

--- index.php
+++ index.php
@@ -1,49 +1,19 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/style.css"/>
-        <title></title>
-    </head>
-    <body>
-        
-        <!-- Button to open the modal login form -->
-        <button onclick="document.getElementById('id01').style.display='block'">Login</button>
+<?php
+  require "db.php";
+?>
+<?php session_start();
+if (array_key_exists("logged_user", $_SESSION)) {
+    echo "Привет " . $_SESSION['logged_user'];
+} else {
+   header('Location:/signup.php');
+   exit;
+}
+?>
+ <!-- если пользовател не выходил с учотной записи пропустить сразу на главную -->
+ <?php if( isset($_SESSION['logged_user'])) : ?>
+     <hr>
+     <a href="/logout.php">Выйти</a>
+ <?php else : ?>
+ <?php endif; ?>
 
-        <!-- The Modal -->
-        <div id="id01" class="modal">
-          <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
 
-          <!-- Modal Content -->
-          <form class="modal-content animate" method="post" action="db/register.php">
-            <div class="imgcontainer">
-                <img src="img/img_avatar2.png" alt="Avatar" class="avatar">
-            </div>
-
-            <div class="container">
-              <label for="uname"><b>Username</b></label>
-              <input type="text" placeholder="Enter Username" name="uname" required>
-
-              <label for="psw"><b>Password</b></label>
-              <input type="password" placeholder="Enter Password" name="psw" required>
-
-              <button type="submit">Login</button>
-              <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-              </label>
-            </div>
-
-            <div class="container" style="background-color:#f1f1f1">
-              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-              <span class="psw">Forgot <a href="#">password?</a></span>
-            </div>
-          </form>
-        </div>
-        
-    </body>
-</html>
